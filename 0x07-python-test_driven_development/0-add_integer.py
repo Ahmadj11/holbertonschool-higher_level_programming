bb4580f02d7624504a8cff4af41b509d89d60e43
Ahmadj11 <ahmadj.file9@gmail.com>
2022-05-18 03:24:13 +0000

    placement for text

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,34 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-    if type(a) is not int:
-        raise TypeError ("a mustt be an integer")
-    if type(b) is not int:
-        raise TypeError ("b must be an integer")
+""" Add integers """
 
+
+def add_integer(a, b=98):
+    """ Add integers
+
+    Args:
+        a (int): [Mandatory int to pass in]
+        b (int): [Optional int to pass in]
+    Returns:
+        int: [Sum of a and b]
+    """
+    try:
+        if isinstance(a, int):
+            pass
+        elif isinstance(a, float):
+            a = round(int(a), 0)
+        else:
+            raise TypeError
+    except Exception:
+        raise TypeError("a must be an integer")
+
+    try:
+        if isinstance(b, int):
+            pass
+        elif isinstance(b, float):
+            b = round(int(b), 2)
+        else:
+            raise TypeError
+    except Exception:
+        raise TypeError("b must be an integer")
+
+    return a + b
