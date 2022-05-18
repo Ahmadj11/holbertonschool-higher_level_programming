6b6edf00518b212520bc354b17c944ae5a962880
Ahmadj11 <ahmadj.file9@gmail.com>
2022-05-18 04:48:34 +0000

    indentation1

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Write a function that prints  \
+    text with 2 new lines after \
+    each of these characters: ., ? and : """
+
+
+def text_indentation(text):
+    """ Write a function that prints text
+
+    Args:  text (str): text to print
+    """
+    if type(text) is not str or text == "":
+        raise TypeError("text must be a string")
+    spec_char = [".", "?", ":"]
+    while True:
+        for ltr in text:
+            if ltr in spec_char:
+                print(ltr)
+                print()
+                print('', end="", flush=True)
+
+            else:
+                print(ltr, end="", flush=True)
+        break
