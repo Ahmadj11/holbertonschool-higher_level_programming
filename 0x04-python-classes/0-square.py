92c3a4fd2f7c509fa1ce26bc5d4a6a0b1c30410f
Ahmadj11 <ahmadj.file9@gmail.com>
2022-05-18 03:18:51 +0000

    pls4

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-""" Simple sqaure """
+""" create a class named Square. """
 
 
-class Sqaure:
-""" class where size is size of the sqaure """
-def __init__(self, size=0):
-	if type(size) != int:
-		raise TypeError("size must be an int")
-	if size < 0:
-		raise ValueError("size must be 0")
-	self.__size = size
-	
+class Square:
+    """ square of a dictated size. """
+    def __init__(self, size):
+        self.__size = size 	
