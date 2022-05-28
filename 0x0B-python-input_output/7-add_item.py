713f53b31b4de31b773496c11577c58dbf139250
Ahmadj11 <ahmadj.file9@gmail.com>
2022-05-27 21:38:28 -0500

    ripper

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """
-    adding arguments to a list, as well as saving them in JSON format.
+Saves arguments to a file.
 """
 import sys
-save_json = __import__("5-save_to_json_file").save_to_json_file
-load_json = __import__("6-load_from_json_file").load_from_json_file
-
-FORM = "add_item.json"
+save = __import__("5-save_to_json_file").save_to_json_file
+load = __import__("6-load_from_json_file").load_from_json_file
 
 try:
-    my_list = load_json(FORM)
-except (FileNotFoundError, TypeError):
+    my_list = load("add_item.json")
+except(TypeError, FileNotFoundError):
     my_list = []
 
-for a in sys.argv[1:]:
-    my_list.append(a)
+for x in sys.argv[1:]:
+    my_list.append(x)
 
-save_json(my_list, FORM)
+save(my_list, "add_item.json")
